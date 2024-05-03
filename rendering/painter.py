73adef381897cb555152c7e48d93da17808d3b26
Robert Małafiejew <01169593@pw.edu.pl>
2024-05-03 17:32:36 +0200

    algorithm improvement

--- rendering/painter.py
+++ rendering/painter.py
@@ -4,9 +4,9 @@ from typing import List
 
 def arr_to_rgb(arr: list) -> tuple:
     pivot = len(arr) // 2
-    r = (int(sum(arr[:pivot])) * 150) % 256
-    g = (int(arr[pivot]) * 150) % 256
-    b = (int(sum(arr[pivot + 1:])) * 150) % 256
+    r = (int(sum(arr[:pivot])) * 170) % 256
+    g = (int(arr[pivot]) * 170) % 256
+    b = (int(sum(arr[pivot + 1:])) * 170) % 256
     return r, g, b
 
 
@@ -15,18 +15,25 @@ class Polygon:
         self.rgb = arr_to_rgb(face)
         self.vertices = np.array(vertices)
 
+    def distance(self):
+        centroid = np.mean(self.vertices[:, :3], axis=0)
+        return np.sqrt(np.sum(centroid ** 2))
+
     def plane_normal(self):
         v1 = self.vertices[0, :3]
         v2 = self.vertices[1, :3]
         v3 = self.vertices[2, :3]
-        return np.cross(v2 - v1, v3 - v1)
+        normal = np.cross(v2 - v1, v3 - v1)
+        if np.dot(normal, v1) < 0.0:
+            normal = -normal
+        return normal
 
 
-def point_in_front_of_plane(point_plane: Polygon, i, normal_plane: Polygon) -> bool:
+# result > 0 means point is in front of the plane, result < 0 means point is behind the plane
+def point_position_by_plane(point_plane: Polygon, i, normal_plane: Polygon) -> bool:
     normal = normal_plane.plane_normal()
-    # print(normal, point_plane.vertices[i], normal_plane.vertices[0])
     vec = point_plane.vertices[i, :3] - normal_plane.vertices[0, :3]
-    return np.dot(normal, vec) >= 0.0
+    return np.dot(normal, vec)
 
 
 def obscures(p: Polygon, q: Polygon) -> bool:
@@ -45,40 +52,46 @@ def obscures(p: Polygon, q: Polygon) -> bool:
     # P is contained wholly in the back half-space of Q
     all_in_back = True
     for i in range(len(p.vertices)):
-        if point_in_front_of_plane(p, i, q):
+        if point_position_by_plane(p, i, q) >= 0.0:
             all_in_back = False
-    if not all_in_back:
+            break
+    if all_in_back:
         return False
 
     # Q is contained wholly in the front half-space of P
     all_in_front = True
     for i in range(len(q.vertices)):
-        if not point_in_front_of_plane(q, i, p):
+        if point_position_by_plane(q, i, p) <= 0.0:
             all_in_front = False
-    if not all_in_front:
+            break
+    if all_in_front:
         return False
 
     return True
 
 
 def sort_by_z(polygons):
-    return sorted(polygons, key=lambda x: np.mean(x.vertices[:, 2]), reverse=True)
+    return sorted(polygons, key=lambda x: x.distance(), reverse=True)
 
 
 def painter_algorithm(polygons: List[Polygon]):
     sorted_polygons = sort_by_z(polygons)
-    draw_stack = []
+    draw_order = []
 
     for polygon in sorted_polygons:
-        # Check if the polygon is visible
-        visible = True
-        for other_polygon in draw_stack:
-            if obscures(polygon, other_polygon):
-                i = draw_stack.index(other_polygon)
-                draw_stack = draw_stack[:i] + [polygon] + draw_stack[i:]
-                visible = False
-                break
-        if visible:
-            draw_stack.append(polygon)
+        draw_order.append(polygon)
+        # i = len(draw_order) - 1
+        # while i > 0 and obscures(draw_order[i-1], draw_order[i]):
+        #     draw_order[i], draw_order[i - 1] = draw_order[i - 1], draw_order[i]
+        #     i -= 1
 
-    return draw_stack
+        not_obstructed = True
+        for other_polygon in draw_order:
+            if obscures(polygon, other_polygon):
+                i = draw_order.index(other_polygon)
+                draw_order = draw_order[:i] + [polygon] + draw_order[i:]
+                not_obstructed = False
+                break
+        if not_obstructed:
+            draw_order.append(polygon)
+    return draw_order
